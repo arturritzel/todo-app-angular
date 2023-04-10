b0d5304ead9c4c788a6688e4cb48d39af4d16dbd
tuisapo <ritzelzim@gmail.com>
2023-04-10 20:23:27 -0300

    adicionado o sistema de adição de tarefas e salvamento local

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.models';
 
 @Component({
@@ -11,14 +12,35 @@ export class AppComponent {
 // publica      tipo: qualquer coisa kk (array pq [])
 //       nome da var    vazio (podia terminar em any[] mas dai a info seria undefined, e no caso, quero vazio)
   public title: String = 'meus afazeres:';
+  public form: FormGroup;
 
 /**
    * ctor > tab
    */
-  constructor() {
-    this.todos.push(new Todo(1, 'fazer carinho no gatinho', false));
-    this.todos.push(new Todo(2, 'quebrar um cabinho (tem que ser com alicate)', false));
-    this.todos.push(new Todo(3, 'morrer atropelado', false));
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(1),
+        Validators.maxLength(50),
+        Validators.required
+      ])]
+    })
+
+    this.load();
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false))
+
+    this.save();
+    this.formClear();
+  }
+
+  formClear(){
+    this.form.reset();
   }
 
   remove(todo: Todo){
@@ -27,13 +49,27 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+
+    this.save();
   }
 
   markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo){
     todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    if(data) this.todos = JSON.parse(data);
   }
 }
