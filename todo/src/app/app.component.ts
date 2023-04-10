2ed95abd3fa54237da8e9cd41e288396a2a8f992
tuisapo <ritzelzim@gmail.com>
2023-04-10 18:20:29 -0300

    primeiro botao (2)

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+// publica      tipo: qualquer coisa kk (array pq [])
+//       nome da var    vazio (podia terminar em any[] mas dai a info seria undefined, e no caso, quero vazio)
+  public title: String = 'meus afazeres:';
+
+/**
+   * ctor > tab
+   */
+  constructor() {
+    this.todos.push('brincar de boneca');
+    this.todos.push('comprar abacate');
+    this.todos.push('tropeçar no meio da rua');
+  }
+
+  alteraTitulo(){
+    this.title = 'meus o que fazer:';
+  }
 }
